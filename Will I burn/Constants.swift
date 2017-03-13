f14b7d437724b1d82f2a29f415f566ecb0c2b63e
dragosrobertn <hello@dragosneagu.com>
2017-03-13 11:17:26 +0000

    Build the Weather URL struct for API use

---
+++ Will I burn/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  Will I burn
+//
+//  Created by Dragos Neagu on 13/03/2017.
+//  Copyright © 2017 Dragos Neagu. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherURL {
+    private let baseURL = "https://api.worldweatheronline.com/premium/v1/weather.ashx"
+    private let key = "92982cb5bc664fe88a0105237171303"
+    private let numDayForecast = "&num_of_days=1"
+    private let format = "&format=json"
+    private var coordstr = ""
+    
+    init(lat: String, long: String) {
+        self.coordstr = "?q=\(lat),\(long)"
+    }
+    
+    func getFullURL() -> String {
+        return baseURL + coordstr + key + numDayForecast + format
+    }
+}
