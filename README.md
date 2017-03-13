fceeae7d644d15308eafbc4aafb69fcf0c74b7b3
dragosrobertn <hello@dragosneagu.com>
2017-03-13 11:17:10 +0000

    Ask for location permission, show alert if denied.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Will I burn?
+
+XCode Project written in Swift using more advanced principles and interacting with
+a RESTful API backend to figure out if the UV Index at the location at which the
+user is currently at will cause them any harm after a prolonged exposure period.
+
+![Will I burn?]()
+# Learning Outcomes
+
+- Interacting with a RESTful API Backend
+- Parsing and working with JSON objects
+- UI Alerts, MapKit and Locations (+ Permissions)
+- HTTP Requests
+
+## Other projects
+
+This project is a part of a few mini [Learning Swift Projects](https://github.com/dragosrobertn/LearningSwiftProjects).
--- Will I burn/ViewController.swift
+++ Will I burn/ViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var coords = CLLocationCoordinate2D(latitude: 53.48, longitude: -2.27)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("Location changed")
+        
+        if status == .authorizedAlways {
+            getLocation()
+        }
+        else if status == .denied {
+            let alert = UIAlertController(title: "Error", message: "You need to enable location services for this app to work correctly. Please go to settings to allow this app to access your location", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func getLocation(){
+        if let loc = locationManager.location?.coordinate {
+            coords = loc
+        }
+    }
 }
 
